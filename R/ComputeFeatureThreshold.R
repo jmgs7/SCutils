7f74ccf410b7c82377b9f6b1afeda01f0699f2b8
jmgs7 <30865599+jmgs7@users.noreply.github.com>
2026-07-14 01:05:57 +0200

    Sync vendor

--- R/ComputeFeatureThreshold.R
+++ R/ComputeFeatureThreshold.R
@@ -80,9 +80,9 @@
 #'     counts.vector    = malat1_norm,
 #'     bw.bandwidth     = 0.01,
 #'     chosen.min       = 2,
-#'     smooth.spar      = 1,
+#'     smooth.spar      = 2,
 #'     abs.min          = 1,
-#'     rough.max        = 6,
+#'     rough.max        = 3,
 #'     conservative.threshold = 2
 #'   )
 #' }
